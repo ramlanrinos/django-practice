a28c51ae314809cda4c082317270934363f9788a
ramlanrinos <e21336@eng.pdn.ac.lk>
2025-06-30 13:39:04 +0530

    command out the logger parts

--- blog/views.py
+++ blog/views.py
@@ -58,13 +58,13 @@ def contact_view(request):
         name = request.POST.get("name")
         email = request.POST.get("email")
         message = request.POST.get("message")
-        logger = logging.getLogger("TESTING")
+        # logger = logging.getLogger("TESTING")
         if form.is_valid():
-            logger.debug(f"POST data is {form.cleaned_data['name']}, {form.cleaned_data['email']}, {form.cleaned_data['message']}")
+            # logger.debug(f"POST data is {form.cleaned_data['name']}, {form.cleaned_data['email']}, {form.cleaned_data['message']}")
             # send email or save in database
             success_msg = "Your email has been sent!"
             return render(request, "contact.html", {"form": form, "success_msg": success_msg})
         else:
-            logger.debug("form validation failure")
+            # logger.debug("form validation failure")
             return render(request, "contact.html", {"form": form, "name": name, "email": email, "message": message})
     return render(request, "contact.html")
